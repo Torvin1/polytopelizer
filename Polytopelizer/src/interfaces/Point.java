470b2bdebdf78e114cf2b934b5fd328700a04636
Max Willert <willerma@zedat.fu-berlin.de>
2014-05-02 10:14:52 +0200

    Point-Definition changed (Points are final object)
    You can use the Point-Class either for 3-dim Objects or for 2-dim
    Objects.

--- Polytopelizer/src/interfaces/Point.java
+++ Polytopelizer/src/interfaces/Point.java
@@ -1,35 +1,31 @@
 package interfaces;
 
 public class Point {
-    private double x, y, z;
+    private final double x, y, z;
+    
+    public Point(){
+        this(0,0,0);
+    }
+    
+    public Point(double x, double y){   // Bjoern and Phil: you have to use this Constructor
+        this(x,y,0);
+    }
 
-    public Point(double x, double y, double z){
+    public Point(double x, double y, double z){ // Henrique and Patrick: this Constructor is for you
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
-    public void setX(double x){
-        this.x = x;
-    }
-
-    public void setY(double y){
-        this.y = y;
-    }
-
-    public void setZ(double z){
-        this.z = z;
-    }
-
-    public double getX(){
+    public double x(){
         return this.x;
     }
 
-    public double getY(){
+    public double y(){
         return this.y;
     }
 
-    public double getZ(){
+    public double z(){
         return this.z;
     }
 }
