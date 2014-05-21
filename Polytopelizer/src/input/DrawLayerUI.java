9db1011929c524708b1f0993f085c0ed39581270
Björn Rathjen <bjoern.rathjen@gmail.com>
2014-05-21 21:02:26 +0200

    cleaned up Input

--- Polytopelizer/src/input/DrawLayerUI.java
+++ Polytopelizer/src/input/DrawLayerUI.java
@@ -8,73 +8,61 @@ import java.awt.Graphics2D;
 import java.awt.geom.Line2D;
 import java.util.ArrayList;
 
-import Geometry.Point2D;
-
 import javax.swing.JComponent;
 import javax.swing.plaf.LayerUI;
 
-import Datastructures.ApollNetwork;
-
 @SuppressWarnings("serial")
 public class DrawLayerUI extends LayerUI<JComponent> {
-    
-    
+
     @Override
     public void paint(Graphics g, JComponent c) {
-//      draws the components
+        // draws the components
         super.paint(g, c);
-//    creates an new graphic in the drawing layer
-      Graphics2D g2 = (Graphics2D) g.create();
-      
-//      ApollonianNetwork dummy = new ApollNetwork(new Point2D(0+offsetX,500+offsetY), new Point2D(500+offsetX,500+offsetY), new Point2D(250+offsetX,67+offsetY));
+        // creates an new graphic in the drawing layer
+        Graphics2D g2 = (Graphics2D) g.create();
 
-//      InputFrame.aN.addNode(300+InputFrame.offsetX, 250+InputFrame.offsetY);  // will be inserted
-//      InputFrame.aN.addNode(200+InputFrame.offsetX, 400+InputFrame.offsetY);    // will be inserted
-//      InputFrame.aN.addNode( 30+InputFrame.offsetX, 400+InputFrame.offsetY);   // won't be inserted
-//      InputFrame.aN.addNode(290+InputFrame.offsetX, 240+InputFrame.offsetY);
-//      
-      
-//    paints the network into the graphic
+        // paints the network into the graphic
 
-      paintApollonianNetwork(g2, InputFrame.aN);
-//      frees the resources after drawing
-      g2.dispose();
+        paintApollonianNetwork(g2, InputFrame.aN);
+        // frees the resources after drawing
+        g2.dispose();
     }
+
     /*
      * TODO fills the drawing layer with the current apollnetwork
      */
-private void paintApollonianNetwork(Graphics2D g, ApollonianNetwork aN) {
-    ArrayList<Face> smallestfaces = new ArrayList<Face>();
-    Face faces = aN.getFaces();
-//  helpfunction to access the outterface
-    smallestfaces.add(faces);
-    if (!faces.isSmallestFace()) 
-        getSmallestFaces(faces, smallestfaces);
-    for (int i = 0 ; i < smallestfaces.size() ; i ++) {
-        paintFace(g, smallestfaces.get(i));
+    private void paintApollonianNetwork(Graphics2D g, ApollonianNetwork aN) {
+        ArrayList<Face> smallestfaces = new ArrayList<Face>();
+        Face faces = aN.getFaces();
+        // helpfunction to access the outterface
+        smallestfaces.add(faces);
+        if (!faces.isSmallestFace())
+            getSmallestFaces(faces, smallestfaces);
+        for (int i = 0; i < smallestfaces.size(); i++) {
+            paintFace(g, smallestfaces.get(i));
+        }      
     }
-//   draws the subfaces of the outter face
-//   TODO add recursive call !!
-    
-}
-private void paintFace(Graphics2D g, Face f) {
-    for (int i = 0; i<f.getPoints().length ; i++) {
-        g.draw(new Line2D.Double(f.getPoints()[i].x(),f.getPoints()[i].y(),f.getPoints()[(i+1)%3].x(), f.getPoints()[(i+1)%3].y()));
-//        g.drawLine((int)(f.getPoints()[i].x()), (int)(f.getPoints()[i].y()), (int)(f.getPoints()[(i+1)%3].x()), ((int)(f.getPoints()[(i+1)%3].y())));
-    }    
-}
-/*
- * TODO error in creating a list with just inner faces
- */
-synchronized private void getSmallestFaces(Face f, ArrayList<Face> smallestfaces) {
-    if (! f.isSmallestFace()) {
-        for (int i = 0 ; i < f.smallerFaces().length ; i++) {
-            getSmallestFaces(f.smallerFaces()[i],smallestfaces);
+
+    private void paintFace(Graphics2D g, Face f) {
+        for (int i = 0; i < f.getPoints().length; i++) {
+            g.draw(new Line2D.Double(f.getPoints()[i].x(),
+                    f.getPoints()[i].y(), f.getPoints()[(i + 1) % 3].x(), f
+                            .getPoints()[(i + 1) % 3].y()));        
         }
-    }else {
-        smallestfaces.add(f);
     }
-}
+
+    /*
+     * TODO error in creating a list with just inner faces
+     */
+    synchronized private void getSmallestFaces(Face f,
+            ArrayList<Face> smallestfaces) {
+        if (!f.isSmallestFace()) {
+            for (int i = 0; i < f.smallerFaces().length; i++) {
+                getSmallestFaces(f.smallerFaces()[i], smallestfaces);
+            }
+        } else {
+            smallestfaces.add(f);
+        }
+    }
 
 }
-
--- Polytopelizer/src/input/InputPanel.java
+++ Polytopelizer/src/input/InputPanel.java
@@ -1,16 +1,15 @@
 package input;
 
 import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
-import javax.swing.JComponent;
 import javax.swing.JPanel;
 
 import Geometry.Point2D;
@@ -22,7 +21,7 @@ import Geometry.Point2D;
 /*########################### imports #######################################*/
 
 @SuppressWarnings("serial")
-public class InputPanel extends JPanel implements MouseListener {
+public class InputPanel extends JPanel implements MouseListener, MouseMotionListener , MouseWheelListener{
 
     /*
      * ########################### attributes
@@ -49,10 +48,9 @@ public class InputPanel extends JPanel implements MouseListener {
         // TODO adjustment, static size ?
         this.setPreferredSize(new java.awt.Dimension(1024, 576));
         // gives panel a border
-        // TODO remove border if no other panel is set
         this.setBorder(BorderFactory.createLineBorder(Color.black));
 
-        addMouseListener(this);
+        addMouseMotionListener(this);
     }
 
     /*
@@ -66,58 +64,13 @@ public class InputPanel extends JPanel implements MouseListener {
     }
 
     /*
-     * check p is a valid point
-     */
-    // private boolean isValid(Point2D p) {
-    // // TODO add isInValidFace
-    // if (isIn(p)) {
-    // return false; // not a valid point
-    // }
-    // return true; // point is valid
-    // }
-    //
-    //
-    //
-    // /*
-    // * check if Point has already been in clicked
-    // * (is already in storage)
-    // * equals cannot be used because p is always a different object
-    // */
-    // private boolean isIn( Point2D p) {
-    //
-    // for (int i = 0;i < storage.size(); i++) {
-    // // comparing the actual values
-    // if (storage.get(i).x() == p.x() && storage.get(i).y() == p.y()) {
-    // return true;
-    // }
-    // }
-    // return false;
-    // }
-
-    /*
-     * adds a point to the storage as long as point is not already in storage
-     */
-
-    private void storageFill(double x, double y) {
-        // check if p is allowed in the appolonian network
-        // if (isValid(p)) {
-        InputFrame.aN.addNode(x, y);
-        System.out.println(storage); // control print
-        // }else {
-        // // TODO case matching to specify what went wrong
-        // System.err.printf("Not a valid point./n");
-        // }
-
-    }
-
-    /*
      * event to add a new point to the network
      */
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        // TODO insert point in aN
-        storageFill(e.getX(), e.getY());
+
+        InputFrame.aN.addNode(e.getX(), e.getY());
         // System.out.printf("clicked @ %d , %d \n",e.getX(),e.getY());
         repaint();
     }
@@ -144,6 +97,24 @@ public class InputPanel extends JPanel implements MouseListener {
     public void mouseExited(MouseEvent e) {
         // TODO Auto-generated method stub
 
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void mouseWheelMoved(MouseWheelEvent e) {
+        // TODO Auto-generated method stub
+        
     }
 
 }
