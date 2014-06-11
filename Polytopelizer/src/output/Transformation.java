1eb9aaa1684d6b16e4ba7071b3668ba069330569
Patrick Winterstein <patrick.winterstein@fu-berlin.de>
2014-06-11 15:59:20 +0200

    test

---
+++ Polytopelizer/src/output/Transformation.java
@@ -0,0 +1,45 @@
+package output;
+
+import interfaces.StackedPolytope;
+import java.util.ArrayList;
+
+public class Transformation {
+    public static void iteratePolytope(StackedPolytope sP, ArrayList<ArrayList<Double>> al, ArrayList<ArrayList<Integer>> al2){
+        ArrayList<Integer> indices = new ArrayList<Integer>();
+        for(int i = 0; i < sP.getPoints().length; i++){
+            ArrayList<Double> a = new ArrayList<Double>();
+            a.add(((Long)sP.getPoints()[i].x().longValue()).doubleValue());
+            a.add(((Long)sP.getPoints()[i].y().longValue()).doubleValue());
+            a.add(((Long)sP.getPoints()[i].z().longValue()).doubleValue());
+            if(!al.contains(a)){
+                al.add(a);
+            }
+            indices.add(al.indexOf(a));
+        }
+        al2.add(indices);
+        
+        iteratePolytopeHelp(sP, al, al2);
+    }
+    
+    private static void iteratePolytopeHelp(StackedPolytope sP, ArrayList<ArrayList<Double>> al, ArrayList<ArrayList<Integer>> al2){
+        if(!sP.isBoundary()){
+            StackedPolytope[] smallerPolytops = sP.smallerPolytopes();
+            for(int i = 0; i < smallerPolytops.length; i++){
+                iteratePolytopeHelp(smallerPolytops[i], al, al2);
+            }
+        }else{
+            ArrayList<Integer> indices = new ArrayList<Integer>();
+            for(int i = 0; i < sP.getPoints().length; i++){
+                ArrayList<Double> a = new ArrayList<Double>();
+                a.add(((Long)sP.getPoints()[i].x().longValue()).doubleValue());
+                a.add(((Long)sP.getPoints()[i].y().longValue()).doubleValue());
+                a.add(((Long)sP.getPoints()[i].z().longValue()).doubleValue());
+                if(!al.contains(a)){
+                    al.add(a);
+                }
+                indices.add(al.indexOf(a));
+            }
+            al2.add(indices);
+        }
+    }
+}
---
+++ Polytopelizer/src/output/ViewerFrame.java
@@ -0,0 +1,87 @@
+package output;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+
+import de.jreality.jogl.Viewer;
+import de.jreality.math.MatrixBuilder;
+import de.jreality.scene.Appearance;
+import de.jreality.scene.Camera;
+import de.jreality.scene.DirectionalLight;
+import de.jreality.scene.IndexedFaceSet;
+import de.jreality.scene.Light;
+import de.jreality.scene.SceneGraphComponent;
+import de.jreality.scene.SceneGraphPath;
+import de.jreality.shader.CommonAttributes;
+import de.jreality.tools.ClickWheelCameraZoomTool;
+import de.jreality.tools.DraggingTool;
+import de.jreality.tools.RotateTool;
+import de.jreality.tools.ScaleTool;
+import de.jreality.toolsystem.ToolSystem;
+import de.jreality.util.RenderTrigger;
+
+@SuppressWarnings("serial")
+public class ViewerFrame extends JFrame{
+    
+    public ViewerFrame(IndexedFaceSet ifs){
+        super();
+        SceneGraphComponent rootNode = new SceneGraphComponent("root");
+        SceneGraphComponent cameraNode = new SceneGraphComponent("camera");
+        SceneGraphComponent geometryNode = new SceneGraphComponent("geometry");
+        SceneGraphComponent lightNode = new SceneGraphComponent("light");
+        
+        rootNode.addChild(geometryNode);
+        rootNode.addChild(cameraNode);
+        cameraNode.addChild(lightNode);
+        
+        Light dl=new DirectionalLight();
+        lightNode.setLight(dl);
+        
+        geometryNode.setGeometry(ifs);
+        
+        RotateTool rotateTool = new RotateTool();
+        geometryNode.addTool(rotateTool);
+        
+        DraggingTool draggingTool = new DraggingTool();
+        geometryNode.addTool(draggingTool);
+
+        ClickWheelCameraZoomTool scaleTool = new ClickWheelCameraZoomTool();
+        rootNode.addTool(scaleTool);
+        
+        MatrixBuilder.euclidean().translate(0, 0, 3).assignTo(cameraNode);
+
+        Appearance rootApp= new Appearance();
+        rootApp.setAttribute(CommonAttributes.BACKGROUND_COLOR, new Color(0f, .1f, .1f));
+        rootApp.setAttribute(CommonAttributes.DIFFUSE_COLOR, new Color(1f, 0f, 0f));
+        rootNode.setAppearance(rootApp);
+            
+        Camera camera = new Camera();
+        cameraNode.setCamera(camera);
+        SceneGraphPath camPath = new SceneGraphPath(rootNode, cameraNode);
+        camPath.push(camera);
+        
+        Viewer viewer = new Viewer();
+        viewer.setSceneRoot(rootNode);
+        viewer.setCameraPath(camPath);
+        ToolSystem toolSystem = ToolSystem.toolSystemForViewer(viewer);
+        toolSystem.initializeSceneTools();
+        
+        this.setVisible(true);
+        this.setSize(640, 480);
+        this.getContentPane().add((Component) viewer.getViewingComponent());
+        this.validate();
+        this.addWindowListener(new WindowAdapter() {
+          public void windowClosing(WindowEvent arg0) {
+            System.exit(0);
+          }
+        });
+        
+        RenderTrigger rt = new RenderTrigger();
+        rt.addSceneGraphComponent(rootNode);
+        rt.addViewer(viewer);
+    }
+}
